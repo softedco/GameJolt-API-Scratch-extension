930653d6e34ec0aa02627db9386cb5d1b1365005
!ryan <100989385+softedco@users.noreply.github.com>
2022-10-27 18:10:02 +0600

    Add files via upload

--- GameJoltAPI.js
+++ GameJoltAPI.js
@@ -621,11 +621,11 @@ class GameJoltAPI {
         if (!GJAPI.bLoggedIn || !GJAPI.bSessionActive) { achievedData[0] = err; return err; }
         GJAPI.TrophyFetch(GJAPI.TROPHY_ONLY_ACHIEVED, function (pResponse) {
             if (!pResponse.trophies) { achievedData[0] = err; return err; }
-            for (i = 0; i <= pResponse.trophies.length; i++) {
+            for (i = 0; i < pResponse.trophies.length; i++) {
                 achievedData[i] = pResponse.trophies[i].id;
             }
         });
-        if ((achievedData[0] ?? true) || achievedData[0] == err) { return err; }
+        if (achievedData[0] == err) { return err; }
         return achievedData.includes(args.ID);
     }
     scoreAdd(args) {
