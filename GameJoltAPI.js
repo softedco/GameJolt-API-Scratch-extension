6b08692b305965c0cff78f5d54b29ac149d54957
!ryan <100989385+softedco@users.noreply.github.com>
2022-10-31 12:34:11 +0600

    Add files via upload

--- GameJoltAPI.js
+++ GameJoltAPI.js
@@ -31,7 +31,7 @@ function md5_hh(b,a,c,d,e,f,g){return md5_cmn(a^c^d,b,a,e,f,g)}function md5_ii(b
 // Extension
 
 let editor = true;
-editor = typeof Scratch.extensions != 'object';
+editor = typeof Scratch != 'object';
 
 if (!editor) {
     const Scratch = {
