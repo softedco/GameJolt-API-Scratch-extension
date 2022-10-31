b2ee0dfd9d4408b420135192f04a607ec5e8dff6
!ryan <100989385+softedco@users.noreply.github.com>
2022-10-31 13:23:33 +0600

    Add files via upload

--- GameJoltAPI.js
+++ GameJoltAPI.js
@@ -32,7 +32,9 @@ function md5_hh(b,a,c,d,e,f,g){return md5_cmn(a^c^d,b,a,e,f,g)}function md5_ii(b
 
 // Extension
 
-if (!editor) {
+var Scratch;
+
+if (Scratch ?? true) {
     var Scratch = {
         BlockType: {
             COMMAND: 'command',
