ed08711bc01026f5ecae7bcb351fc5b5d585779b
!Ryan <100989385+softedco@users.noreply.github.com>
2022-11-03 16:11:13 +0600

    Test

--- GameJoltAPI.js
+++ GameJoltAPI.js
@@ -169,7 +169,8 @@ GJAPI.ScoreFetchGuestEx = function (iScoreTableID, bName, iLimit, iBetterOrWorse
 /* Sandbox detection by MikeDev101
  * Can be used outside of this extension with the unsandboxing script
  */
-const sandboxed = (typeof window == 'undefined' || !window.vm);
+const sandboxed = true;
+    /* (typeof window == 'undefined' || !window.vm); */
 
 /* Unsandboxing script by softed
  * Can be used outside of this extension
