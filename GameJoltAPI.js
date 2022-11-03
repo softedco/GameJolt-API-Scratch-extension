0a37a35c044995a71f7d8c2dbf1c6b71090fcb23
!Ryan <100989385+softedco@users.noreply.github.com>
2022-11-03 17:23:37 +0600

    Add files via upload

--- GameJoltAPI.js
+++ GameJoltAPI.js
@@ -225,7 +225,7 @@ if (!sandboxed) {
  */
 let err = {
     get(code) {
-        if (err[code] ?? true) {
+        if (err?.[code] == undefined) {
         return 'Error.';
         }
         return 'Error: ' + err[code];
