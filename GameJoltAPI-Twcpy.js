22893f3541445bf6e499cf3f281fba38bcbb6efb
Not actually Ryan <f377994@gmail.com>
2022-11-30 15:19:29 +0600

    changes

--- GameJoltAPI-Twcpy.js
+++ GameJoltAPI-Twcpy.js
@@ -1651,8 +1651,7 @@
 			return GameJolt.bOnGJ;
 		}
 		setGame(args) {
-			GameJolt.iGameID = args.ID;
-			GameJolt.sGameKey = args.key;
+			GameJolt.iGameID = args.ID; GJAPI.sGameKey = args.key;
 		}
 		session(args) {
 			GameJolt.SessionSetStatus(args.openOrClose);
@@ -1665,12 +1664,12 @@
 			GameJolt.SessionPing();
 		}
 		sessionBool() {
-			GameJolt.SessionCheck((pResponse) => {
+			GameJolt.SessionCheck(pResponse => {
 				if (pResponse.message) { err.session = pResponse.message; return; }
 				if (pResponse.success == bool.f) { data.session = false; return; }
 				data.session = true;
 			});
-			if (data.session == undefined) { return err.get("session"); }
+			if (!data.session) return err.get('session');
 			return data.session;
 		}
 
@@ -1682,7 +1681,7 @@
 		}
 
 		/**
-		 * Not necessary since the library handles loggin in for you
+		 * Not necessary since the library handles logging in for you
 		 */
 		loginAuto() {
 			GameJolt.UserLoginAuto();
@@ -1691,33 +1690,33 @@
 			GameJolt.UserLogout();
 		}
 		loginBool() {
-			return GameJolt.bLoggedIn;
+			return GJAPI.bLoggedIn;
 		}
 		userFetch(args) {
-			GameJolt.UserFetchComb(args.fetchType, args.usernameOrID, (pResponse) => {
+			GameJolt.UserFetchComb(args.fetchType, args.usernameOrID, pResponse => {
 				if (pResponse.success == bool.f) { err.user = pResponse.message; return; }
 				data.user = pResponse.users[0];
 			});
 		}
 		userFetchCurrent() {
-			GameJolt.UserFetchCurrent((pResponse) => {
+			GameJolt.UserFetchCurrent( pResponse => {
 				if (pResponse.success == bool.f) { err.user = pResponse.message; return; }
 				data.user = pResponse.users[0];
 			});
 		}
 		returnUserData(args) {
-			if (typeof data.user != "object") { return err.get("user"); }
-			data.user[args.userDataType] = data.user[args.userDataType] ?? err.get("user");
+			if (!data.user) return err.get('user');
+			data.user[args.userDataType] = data.user[args.userDataType] ?? err.get('user');
 			return data.user[args.userDataType];
 		}
 		friendsFetch(args) {
-			GameJolt.FriendsFetch((pResponse) => {
+			GameJolt.FriendsFetch(pResponse => {
 				if (pResponse.success == bool.f) { err.friends = pResponse.message; return; }
 				data.friends = pResponse.friends;
 			});
-			if (typeof data.friends != "object") { return err.get("friends"); }
-			if (typeof data.friends[args.index] != "object") { return err.get("friends"); }
-			data.friends[args.index].friend_id = data.friends[args.index].friend_id ?? err.get("friends");
+			if (!data.friends) return err.get('friends');
+			if (!data.friends[args.index]) return err.get('friends');
+			data.friends[args.index].friend_id = data.friends[args.index].friend_id ?? err.get('friends');
 			return data.friends[args.index].friend_id;
 		}
 		trophyAchieve(args) {
@@ -1727,134 +1726,110 @@
 			GameJolt.TrophyRemove(args.ID);
 		}
 		trophyFetch(args) {
-			GameJolt.TrophyFetchComb(
-				args.indexOrID,
-				args.indexOrID ? GameJolt.TROPHY_ALL : args.value,
-				(pResponse) => {
-					if (pResponse.success == bool.f) { err.trophies = pResponse.message; return; }
-					data.trophies = args.indexOrID ? pResponse.trophies : pResponse.trophies[0];
-				}
-			);
-			if (typeof data.trophies != "object") { return err.get("trophies"); }
+			GameJolt.TrophyFetchComb(args.indexOrID, args.indexOrID ? GJAPI.TROPHY_ALL : args.value, pResponse => {
+				if (pResponse.success == bool.f) { err.trophies = pResponse.message; return; }
+				data.trophies = args.indexOrID ? pResponse.trophies : pResponse.trophies[0];
+			});
+			if (!data.trophies) return err.get('trophies');
 			if (args.indexOrID) {
-				if (typeof data.trophies[args.value] != "object") { return err.get("trophies"); }
-				data.trophies[args.value][args.trophyDataType] = data.trophies[args.value][args.trophyDataType] ?? err.get("trophies");
+				if (!data.trophies[args.value]) return err.get('trophies');
+				data.trophies[args.value][args.trophyDataType] = data.trophies[args.value][args.trophyDataType] ?? err.get('trophies');
 				return data.trophies[args.value][args.trophyDataType];
 			}
-			data.trophies[args.trophyDataType] = data.trophies[args.trophyDataType] ?? err.get("trophies");
+			data.trophies[args.trophyDataType] = data.trophies[args.trophyDataType] ?? err.get('trophies');
 			return data.trophies[args.trophyDataType];
 		}
 		scoreAdd(args) {
 			GameJolt.ScoreAdd(args.ID, args.value, args.text, args.extraData);
 		}
 		scoreAddGuest(args) {
-			GameJolt.ScoreAddGuest(
-				args.ID,
-				args.value,
-				args.text,
-				args.username,
-				args.extraData
-			);
+			GameJolt.ScoreAddGuest(args.ID, args.value, args.text, args.username, args.extraData);
 		}
 		scoreFetch(args) {
-			GameJolt.ScoreFetchEx(
-				args.ID,
-				(args.globalOrPerUser == GameJolt.DATA_STORE_GLOBAL)
-				? GameJolt.SCORE_ALL : GameJolt.SCORE_ONLY_USER,
-				args.amount,
-				args.betterOrWorse,
-				args.value,
-				(pResponse) => {
-					if (pResponse.success == bool.f) { err.scores = pResponse.message; return; }
-					data.scores = pResponse.scores;
-				}
-			);
+			GameJolt.ScoreFetchEx(args.ID,
+			args.globalOrPerUser == GJAPI.DATA_STORE_GLOBAL ? GJAPI.SCORE_ALL : GJAPI.SCORE_ONLY_USER,
+			args.amount,
+			args.betterOrWorse,
+			args.value, pResponse => {
+				if (pResponse.success == bool.f) { err.scores = pResponse.message; return; }
+				data.scores = pResponse.scores;
+			});
 		}
 		scoreFetchGuest(args) {
-			GameJolt.ScoreFetchGuestEx(
-				args.ID,
-				args.username,
-				args.amount,
-				args.betterOrWorse,
-				args.value,
-				(pResponse) => {
-					if (pResponse.success == bool.f) { err.scores = pResponse.message; return; }
-					data.scores = pResponse.scores;
-				}
-			);
+			GameJolt.ScoreFetchGuestEx(args.ID,
+			args.username, args.amount,
+			args.betterOrWorse,
+			args.value, pResponse => {
+				if (pResponse.success == bool.f) { err.scores = pResponse.message; return; }
+				data.scores = pResponse.scores;
+			});
 		}
 		returnScoreData(args) {
-			if (typeof data.scores != "object") { return err.get("scores"); }
-			if (typeof data.scores[args.index] != "object") { return err.get("scores"); }
-			if (args.scoreDataType == "user") {
+			if (!data.scores) return err.get('scores');
+			if (!data.scores[args.index]) return err.get('scores');
+			if (args.scoreDataType == 'user') {
 				if (!data.scores[args.index].user) {
-					data.scores[args.index].guest = data.scores[args.index]?.guest ?? err.get("scores");
+					data.scores[args.index].guest = data.scores[args.index]?.guest ?? err.get('scores');
 					return data.scores[args.index].guest;
 				}
 			}
-			data.scores[args.index][args.scoreDataType] = data.scores[args.index][args.scoreDataType] ?? err.get("scores");
+			data.scores[args.index][args.scoreDataType] = data.scores[args.index][args.scoreDataType] ?? err.get('scores');
 			return data.scores[args.index][args.scoreDataType];
 		}
 		scoreGetRank(args) {
-			GameJolt.ScoreGetRank(args.ID, args.value, (pResponse) => {
+			GameJolt.ScoreGetRank(args.ID, args.value, pResponse => {
 				if (pResponse.success == bool.f) { err.rank = pResponse.message; return; }
 				data.rank = pResponse.rank;
 			});
-			data.rank = data.rank ?? err.get("rank");
+			data.rank = data.rank ?? err.get('rank');
 			return data.rank;
 		}
 		scoreGetTables(args) {
-			GameJolt.ScoreGetTables((pResponse) => {
+			GameJolt.ScoreGetTables(pResponse => {
 				if (pResponse.success == bool.f) { err.tables = pResponse.message; return; }
 				data.tables = pResponse.tables;
 			});
-			if (typeof data.tables != "object") { return err.get("tables"); }
-			if (typeof data.tables[args.index] != "object") { return err.get("tables"); }
-			data.tables[args.index][args.tableDataType] = data.tables[args.index][args.tableDataType] ?? err.get("tables");
+			if (!data.tables) return err.get('tables');
+			if (!data.tables[args.index]) return err.get('tables');
+			data.tables[args.index][args.tableDataType] = data.tables[args.index][args.tableDataType] ?? err.get('tables');
 			return data.tables[args.index][args.tableDataType];
 		}
 		dataStoreSet(args) {
 			GameJolt.DataStoreSet(args.globalOrPerUser, args.key, args.data);
 		}
 		dataStoreFetch(args) {
-			GameJolt.DataStoreFetch(args.globalOrPerUser, args.key, (pResponse) => {
+			GameJolt.DataStoreFetch(args.globalOrPerUser, args.key, pResponse => {
 				if (pResponse.success == bool.f) { err.store = pResponse.message; return; }
 				data.store = pResponse.data;
 			});
-			data.store = data.store ?? err.get("store");
+			data.store = data.store ?? err.get('store');
 			return data.store;
 		}
 		dataStoreUpdate(args) {
-			GameJolt.DataStoreUpdate(
-				args.globalOrPerUser,
-				args.key,
-				args.operationType,
-				args.value
-			);
+			GameJolt.DataStoreUpdate(args.globalOrPerUser, args.key, args.operationType, args.value);
 		}
 		dataStoreRemove(args) {
 			GameJolt.DataStoreRemove(args.globalOrPerUser, args.key);
 		}
 		dataStoreGetKey(args) {
-			GameJolt.DataStoreGetKeysEx(args.globalOrPerUser, args.pattern, (pResponse) => {
+			GameJolt.DataStoreGetKeysEx(args.globalOrPerUser, args.pattern, pResponse => {
 				if (pResponse.success == bool.f) { err.keys = pResponse.message; return; }
 				data.keys = pResponse.keys;
 			});
-			if (typeof data.keys != "object") { return err.get("keys"); }
-			if (typeof data.keys[args.index] != "object") { return err.get("keys"); }
-			data.keys[args.index].key = data.keys[args.index].key ?? err.get("keys");
+			if (!data.keys) return err.get('keys');
+			if (!data.keys[args.index]) return err.get('keys');
+			data.keys[args.index].key = data.keys[args.index].key ?? err.get('keys');
 			return data.keys[args.index].key;
 		}
 		timeFetch(args) {
-			GameJolt.TimeFetch((pResponse) => {
+			GameJolt.TimeFetch(pResponse => {
 				if (pResponse.success == bool.f) { err.time = pResponse.message; return; }
 				data.time = pResponse;
 			});
-			if (typeof data.time != "object") { return err.get("time"); }
-			data.time[args.timeType] = data.time[args.timeType] ?? err.get("time");
+			if (!data.time) return err.get('time');
+			data.time[args.timeType] = data.time[args.timeType] ?? err.get('time');
 			return data.time[args.timeType];
 		}
 	}
 	Scratch.extensions.register(new GameJoltAPI());
-
 })(Scratch);
